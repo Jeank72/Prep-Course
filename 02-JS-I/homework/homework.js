ae67128baf6b9ca1fbdbbf206f00defcab25312d
jeank72 <jeannsequea@hotmail.com>
2022-05-05 17:11:08 -0500

    prueba

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,16 +7,16 @@ const nuevaString = "carro";
 const nuevoNum = 12;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = tue;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1.05;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,14 +28,17 @@ const nuevoModulo = 21 % 5 === null;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  return str;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  x=4;
+  y=2;
+  suma=x+y;
+  return suma;
 }
 
 function resta(x, y) {
